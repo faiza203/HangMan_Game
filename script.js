11112291167bf70df4ff3eb2b771c19548898c12
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-04-22 12:20:45 +0500

    completed

--- script.js
+++ script.js
@@ -12,3 +12,81 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 let playable = true;
 const correctLetters = [];
 const wrongLetters = [];
+function displayWord() {
+	wordEl.innerHTML = `
+    ${selectedWord
+			.split('')
+			.map(
+				letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+			)
+			.join('')}
+  `;
+	const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = 'Congratulations! You won! 😃';
+		popup.style.display = 'flex';
+		playable = false;
+	}
+}
+function updateWrongLettersEl() {
+	wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
+if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = 'Unfortunately you lost. 😕';
+		finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
+		popup.style.display = 'flex';
+		playable = false;
+	}
+}
+function showNotification() {
+	notification.classList.add('show');
+	setTimeout(() => {
+		notification.classList.remove('show');
+	}, 2000);
+}
+window.addEventListener('keydown', e => {
+	if (playable) {
+		if (e.keyCode >= 65 && e.keyCode <= 90) {
+			const letter = e.key.toLowerCase();
+			if (selectedWord.includes(letter)) {
+				if (!correctLetters.includes(letter)) {
+					correctLetters.push(letter);
+					displayWord();
+				} else {
+					showNotification();
+				}
+			} else {
+				if (!wrongLetters.includes(letter)) {
+					wrongLetters.push(letter);
+					updateWrongLettersEl();
+				} else {
+					showNotification();
+				}
+			}
+		}
+	}
+});
+playAgainBtn.addEventListener('click', () => {
+	playable = true;
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+	displayWord();
+	updateWrongLettersEl();
+	popup.style.display = 'none';
+});
+displayWord();
